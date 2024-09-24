412b664cab40501247d498dc575423662e5b7797
seojeong <seojeong1101@gmail.com>
2024-09-24 16:41:53 +0900

    https 변경 / ssl 인증서 추가

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import fs from "fs";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: "/aiesc/",
+  // plugins: [vue()],
   base: "/aiesc/",
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "cert/localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "cert/localhost.pem")),
+    },
+    host: "localhost",
+    port: 3000, // 원하는 포트 번호
+  },
   plugins: [vue()],
 });
